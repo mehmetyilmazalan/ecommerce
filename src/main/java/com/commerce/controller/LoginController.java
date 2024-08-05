81de7b1ba939952c4ff71c1588ab2af1a268b1c9
Your Name <you@example.com>
2024-08-05 16:30:18 +0300

    Dosyaların içeriğini ekle

--- src/main/java/com/commerce/controller/LoginController.java
+++ src/main/java/com/commerce/controller/LoginController.java
@@ -1,2 +1,15 @@
-package com.commerce.controller;public class LoginController {
+package com.commerce.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+
+@Controller
+public class LoginController {
+    public String login(Model model) {
+        model.addAttribute("user", "Merhaba");
+        return "login";
+
+    }
+
 }
+
